cd12c841fdaa5a0c0f87e010c6cc81ca78ef8e24
Steven Camara <stevengdc@users.noreply.github.com>
2017-02-16 11:28:19 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -9,19 +9,17 @@ var tracker_selectedText = "";
 var preferredNotificationAddress = "";
 var solution_value = "";
 var fullDescription = "";
-
 var emailSubject = "";
 var emailGreeting = "";
+var callDetails = "";
+var emailBody = "";
+var referenceNumber = "";
+
 var escalationText = new Array (
 	"      Customer called to chase this call and wants to escalate it, can anyone take a look at this please, thanks.",
 	"      Message 2",
 	"      Message 3"
 );
-var callDetails = "";
-var emailBody = "";
-
-var referenceNumber = "";
-
 
 function timeGreeting() {
     var n = new Date().getHours();
@@ -31,11 +29,11 @@ function timeGreeting() {
 
 // This callback function is called when the content script has been injected and returned its results
 function onPageDetailsReceived(pageDetails)  {
-	//pageDetails.title
+    //pageDetails.title
     document.getElementById('title').value = pageDetails.assignee_selectedText;
     document.getElementById('url').value = pageDetails.url; 
     document.getElementById('summary').innerText = pageDetails.summary;
-	requestNumber = pageDetails.requestNumber;
+    requestNumber = pageDetails.requestNumber;
     contact_telephone = pageDetails.contact_telephone;
     spokeTo_name = pageDetails.spokeTo_name;
     location_selectedText = pageDetails.location_selectedText;
@@ -45,29 +43,26 @@ function onPageDetailsReceived(pageDetails)  {
     tracker_selectedText = pageDetails.tracker_selectedText;
     preferredNotificationAddress = pageDetails.preferredNotificationAddress;
     solution_value = pageDetails.solution_value;
-	fullDescription = pageDetails.summary;
+    fullDescription = pageDetails.summary;
 
-	emailSubject = "Marval Call " + requestNumber + " Escalation";
-	emailGreeting = "Hello, " + timeGreeting() + " dear " + assignee_selectedText + ",\n\n\t";
-	callDetails = "\n\nCall details:\n      Customer name: " + spokeTo_name + "\n      Contact Telephone: " + contact_telephone + "\n      Address: " + location_selectedText + "\n      Service: " + service_selectedText + "\n      Short Description: "
-        + description + "\n      Assignee: " + assignee_selectedText + "\n      Customer Email: " + preferredNotificationAddress;
+    emailSubject = "Marval Call " + requestNumber + " Escalation";
+    emailGreeting = "Hello, " + timeGreeting() + " dear " + assignee_selectedText + ",\n\n\t";
+    callDetails = "\n\nCall details:\n      Customer name: " + spokeTo_name + "\n      Contact Telephone: " + contact_telephone + "\n      Address: " + location_selectedText + "\n      Service: " + service_selectedText + "\n      Short Description: "
+    + description + "\n      Assignee: " + assignee_selectedText + "\n      Customer Email: " + preferredNotificationAddress;
 
     if (fullDescription != "") {
 		callDetails = callDetails + "\n\nFull Description:\n\n" + fullDescription;
-	}
+    }
 	
-	var greeting = timeGreeting();
+    var greeting = timeGreeting();
     emailBody = "Hello, " + greeting + " dear " + assignee_selectedText + ",\n\n\tCustomer called to chase this call and wants to escalate it, can anyone take a look at this please, thanks.\n\nCall details:\n\tCustomer name: "
         + spokeTo_name + "\n\tContact Telephone: " + contact_telephone + "\n\tAddress: " + location_selectedText + "\n\tService: " + service_selectedText + "\n\tShort Description: "
         + description + "\n\tAssignee: " + assignee_selectedText + "\n\tCustomer Email: " + preferredNotificationAddress;
     if (fullDescription != "") {
-		emailBody = emailBody + "\n\nFull Description:\n\n\t" + fullDescription;
+	    emailBody = emailBody + "\n\nFull Description:\n\n\t" + fullDescription;
 	}
 	document.getElementById('email').innerText = emailBody;
-}
-
-// Global reference to the status display SPAN
-var statusDisplay = null;
+    }
 
 // URL the data via mailto to default email client
 function addBookmark() {
